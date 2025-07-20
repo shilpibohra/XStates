ca02e1c7d92db3e1a897c038f82d60e84f19a916
shilpi bohra <shilpibohra@gmail.com>
2025-07-21 00:00:08 +0530

    Your message here

---
+++ xstates/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import CitySelector from "./components/CitySelector";
+
+function App() {
+  return (
+    <div>
+      <CitySelector />
+    </div>
+  );
+}
+
+export default App;
